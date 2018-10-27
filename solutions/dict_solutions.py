782e3af660155bab3106f7a4322360c6f09b7172
MJ <manoj.kumar31028@gmail.com>
2018-10-27 14:59:45 -0600

    Update dict_solutions.py

--- solutions/dict_solutions.py
+++ solutions/dict_solutions.py
@@ -21,6 +21,24 @@ class PythonDict(object):
             mydict[heroes[i]] = movies[i]
         return mydict
 
+    def iterate_and_print_using_keys(self):
+
+        pass
+
+    def get_key(self, movie_count):
+        """Note that multiple keys can have same value. If you find mutiple 
+        keys with same value then return list of all matching keys
+        Example: If I pass movie_count as 3, it should return a list [
+        'allu','kamal'] . If I pass value as 7, it should return only a 
+        string 'bala'. Ideally, a function should return same type. It is 
+        bad programming to return different types in different cases. As 
+        this for learning purposes, we can ignore"""
+        pass
+
+    def get_value(self):
+        pass
+    
+
 if __name__=="__main__":
     obj = PythonDict(20) # if you do not pass one value to the class
     # intalization it will throw an argument error
