35f983158bdb9f2801a52cccc14eecb83e176763
MJ <manoj.kumar31028@gmail.com>
2018-10-27 15:06:00 -0600

    Update dict_solutions.py

--- solutions/dict_solutions.py
+++ solutions/dict_solutions.py
@@ -33,11 +33,48 @@ class PythonDict(object):
 
     def iterate_and_print_using_keys(self):
         """
-        
+
         Returns:
 
         """
 
+        pass
+
+    def get_heroes(self, movie_count):
+        """Note that multiple keys can have same value. If you find mutiple
+        keys with same value then return list of all matching keys
+        Example: If I pass movie_count as 3, it should return a list [
+        'allu','kamal'] . If I pass value as 7, it should return only a
+        string 'bala'. Ideally, a function should return same type. It is
+        bad programming to return different types in different cases. As
+        this for learning purposes, we can ignore"""
+        pass
+
+    def get_movie_count(self, hero):
+        """
+        Here the function description goes
+        Args:
+            hero (type of argument): definition of argument
+
+        Returns:
+            Mention what it returns and what type
+            
+        This is the proper way of documenting a function as per PEP8 rules
+        Use this for all functions
+        """
+        # you can have duplicate keys. keys are like hash values which are
+        # always unique in dict. Above function get_heros() may have 
+        # duplicate values, but this function will return a uniqe vakye
+        pass
+
+
+if __name__=="__main__":
+    obj = PythonDict(20) # if you do not pass one value to the class
+    # intalization it will throw an argument error
+    # print the class variables
+    print(obj.new_value) # these are variables not functions, so do not use
+    # obj.new_value() is wrong
+    print(obj.adict)
         pass
 
     def get_heroes(self, movie_count):
